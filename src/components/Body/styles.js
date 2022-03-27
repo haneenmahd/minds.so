8e6e432f1ab236795aa52e7d29d176c265f731b6
Haneen Mahdin <haneenmahdin@gmail.com>
2022-03-27 09:02:17 +0530

    Add default font size for children of Body component

--- src/components/Body/styles.js
+++ src/components/Body/styles.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 const Style = styled.article`
     padding: ${p => p.padding};
     margin: ${p => p.margin};
+    font-size: larger;
 `;
 
 export default Style;
