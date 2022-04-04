a96fde968d12db0bc2202596f7c329b96de1c339
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-04 15:24:55 +0530

    Rename prop to `action`

--- src/components/QuickAction/index.jsx
+++ src/components/QuickAction/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Style from "./styles";
 
-export default function QuickAction({ clickAction, children }) {
-  return <Style onClick={clickAction}>{children}</Style>;
+export default function QuickAction({ action, children }) {
+  return <Style onClick={action}>{children}</Style>;
 }
